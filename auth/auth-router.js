0361165d1607330ad058d3d974f883e914e92df3
Ba-Zen <bazenatlas@gmail.com>
2019-06-11 20:37:29 -0400

    incorporated knex session store

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -15,7 +15,7 @@ router.post('/register', (req, res) => {
         res.status(201).json(saved);
     })
     .catch(err => {
-        res.status(500),json(err);
+        res.status(500).json(err);
     });
 })
 
